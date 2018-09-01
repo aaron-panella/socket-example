323bb0cc1eef28a3a0e9d980687160b763e15c71
Aaron <a.panella.aus@gmail.com>
2018-09-01 17:04:49 +0930

    Adding working CMake files for client and server for boost

---
+++ client/build/CMakeFiles/FFDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FFDetection.dir/main.cpp.o"
+  "FFDetection.pdb"
+  "FFDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FFDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "boost/asio.hpp"
+#include <thread>
+#include <string>
+
+
+int main()
+{
+    return 0;
+}
